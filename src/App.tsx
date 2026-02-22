7ff3425ae25e53560c5dc7213a582fdfd7d2b3c9
Hyder Khan <105720909+hydermbs@users.noreply.github.com>
2026-02-22 15:55:24 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './components/header';
+import MidBody from './components/midBody/MidBody';
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <MidBody/>
+    </>
+  )
+}
+
+export default App
